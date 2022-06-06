529397af2e072e4a72f914d656ce3c3457466053
JAGAN44QA <itzjagan4mail@gmail.com>
2022-06-06 14:39:51 +0530

    Third commit - For Loop

--- abc.java
+++ abc.java
@@ -2,6 +2,9 @@ public class Abc
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hello World");
+		for(int i=0; i<10; i++)
+		{
+			System.out.println("Just for Demo");
+		}
 	}
 }
